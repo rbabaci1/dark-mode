bd79002b7e91ea120b261936f974c76e0036b076
rabah babaci <4rabah@gmail.com>
2020-03-16 21:31:30 -0700

    create a setter function inside useLocalStorage that sets a value to state an localStorage

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 
 export const useLocalStorage = (key, initialValue) => {
+  // the value part
   const [storedValue, setStoredValue] = useState(() => {
     const item = localStorage.getItem(key);
     return item ? JSON.parse(item) : initialValue;
   });
+
+  // the setter part
+  const setItem = value => {
+    setStoredValue(value);
+    localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setItem];
 };
