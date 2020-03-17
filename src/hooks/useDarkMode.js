9dca777145b8f49041f1cdf32b8c0167ea9f192e
rabah babaci <4rabah@gmail.com>
2020-03-16 21:45:43 -0700

    return onDarkMode and setOnDarkMode form useDarkMode custom hook

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,13 +1,15 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = () => {
-  const [OnDarkMode, setOnDarkMode] = useLocalStorage('darkModed', false);
+  const [OnDarkMode, setOnDarkMode] = useLocalStorage('onDarkMode', false);
 
   useEffect(() => {
     OnDarkMode
       ? document.body.classList.add('dark-mode')
       : document.body.classList.remove('dark-mode');
   }, [OnDarkMode]);
+
+  return [setOnDarkMode];
 };
