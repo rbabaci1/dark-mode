6ffd2a59b955b78b775f3bf6d906cc8f3fe19b66
rabah babaci <4rabah@gmail.com>
2020-03-18 16:24:16 -0700

    add some data to the charts and adjust the CSS depending on the current mode

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -28,13 +28,15 @@ const Chart = ({ sparkLineData }) => {
     .filter(data => data);
 
   return (
-    <LineChart width={1100} height={300} data={formattedData}>
-      <Line type='monotone' dataKey='value' stroke='#8884d8' />
-      <CartesianGrid stroke='#ccc' strokeDasharray='5 5' />
-      <XAxis dataKey='date' interval={3} />
-      <YAxis />
-      <Tooltip />
-    </LineChart>
+    <>
+      <LineChart width={1000} height={350} data={formattedData}>
+        <Line type='monotone' dataKey='value' stroke='#8884d8' />
+        <CartesianGrid stroke='gray' strokeDasharray='5 5' />
+        <XAxis dataKey='date' interval={3} />
+        <YAxis />
+        <Tooltip />
+      </LineChart>
+    </>
   );
 };
 
--- src/components/Charts.jsx
+++ src/components/Charts.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Chart from './Chart';
+import { configure } from '@testing-library/react';
 
 const Charts = ({ coinData }) => {
+  console.log(coinData);
   return (
     <div className='charts'>
       {coinData.map(coin => (
@@ -11,6 +13,23 @@ const Charts = ({ coinData }) => {
           <div className='coin__logo'>
             <img src={coin.image} height='40' alt={coin.name} />
           </div>
+
+          <div className='l-h'>
+            <div className='low-high'>
+              <h2 className='low'>
+                Low 24h <span>${coin.low_24h}</span>
+              </h2>
+              <h2 className='high'>
+                High 24h <span>${coin.high_24h}</span>
+              </h2>
+            </div>
+
+            <h2>
+              Current price{' '}
+              <span className='current'>${coin.current_price}</span>
+            </h2>
+          </div>
+
           <Chart sparkLineData={coin.sparkline_in_7d.price} />
         </div>
       ))}
